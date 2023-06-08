d2fb276942ae12d09d50119edf99bc8c9a75e1d9
Bhanu-Ganga-DevOPs <Kittu@225>
2023-06-07 23:00:14 -0400

    ansible playbook for payment payment.service added parameters required into ssm

--- main.tf
+++ main.tf
@@ -23,10 +23,10 @@ resource "aws_ssm_parameter" "parameters" {
 resource "aws_ssm_parameter" "passwords" {
 
   count  = length(var.passwords)
-  name   = var.parameters[count.index].name
+  name   = var.passwords[count.index].name
   type   = "SecureString"
   key_id = "778981c1-c82b-4f3a-aa0e-91dedf2cc6da"
-  value  = var.parameters[count.index].value
+  value  = var.passwords[count.index].value
 }
 
 #resource "aws_ssm_parameter" "parameters" {
